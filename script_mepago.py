65b2e752213e88e64e08b9cf528b6ae19c153e23
Nicocarello <43417894+Nicocarello@users.noreply.github.com>
2025-10-15 11:00:03 -0300

    Update script_mepago.py

--- script_mepago.py
+++ script_mepago.py
@@ -12,11 +12,13 @@ from io import BytesIO
 import base64
 from email.mime.image import MIMEImage
 from zoneinfo import ZoneInfo
-import google.generativeai as genai
+# import google.generativeai as genai
 import re
 import json
 import math
 import time
+from google import genai
+
 
 # === Config desde entorno (poner en GitHub Secrets) ===
 APIFY_TOKEN = os.getenv("APIFY_TOKEN") # <-- definir en GitHub Secrets
